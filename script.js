f6fdb60a89d3ceebbd8305a84d1e40028a70eb20
Gabriel RiCharde <gabe.richarde@my.wheaton.edu>
2016-02-20 03:25:37 +0100

    Alex the Grey Wizard fixed my shit. API calls now work for near & daysFromNow

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 var runButton = document.querySelector('#submitButton');
 
-runButton.addEventListener('click', function() {
-  console.log(1);
-  var myForm = document.querySelector('form');
-  formData = new FormData(myForm);
-  console.log(formData);;
+runButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  var data = {
+  near: document.querySelector("#near").value,
+  daysFromNow: document.querySelector("#DaysFromNow").value  
+}
+
+var request  = { // can be 'url string' or object: 
+  url          : 'http://private-3d4d82-googlemoviesscraper.apiary-mock.com/movies',
+  // method    : 'POST' // [optional] (defaults to 'GET') 
+  body      : data // [optional] payload data could be <formdata> or {key:val}'s or anything 
+  //headers      : {} // [optional] (defaults to '{}' or in case of 'POST': 
+                   // {'X-Requested-With':'XMLHttpRequest','Content-Type':'application/x-www-form-urlencoded' } ) 
+}
+
+minixhr(request, function (received) { console.log(received) })
+console.log(data);
 });
-
-
-// Sending form data to double dips API
-
-var request = new XMLHttpRequest();
-	method = 'GET';
-	url = 'http://private-3d4d82-googlemoviesscraper.apiary-mock.com/movies';
-
-request.open(method, url);
-
-request.onreadystatechange = function () {
-  if (this.readyState === true) {
-    console.log('Boston', this.near);
-    console.log('', this.date);
-
-  }
-  if (xmlhttp.status !== 200) {
-        return;
-    }
-  console.log(xmlhttp.responseText);
-};
-
-request.send();
